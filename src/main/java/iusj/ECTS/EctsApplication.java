1a27a559b2399f8d8e35f9fb19764fe4a22190e4
Teguia-Abel-Pycoder <teguia.dimitri@institutsaintjean.org>
2025-01-18 20:10:58 +0100

    First commit

---
+++ src/main/java/iusj/ECTS/EctsApplication.java
@@ -0,0 +1,13 @@
+package iusj.ECTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EctsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EctsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iusj/ECTS/EctsApplicationTests.java
@@ -0,0 +1,13 @@
+package iusj.ECTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EctsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
