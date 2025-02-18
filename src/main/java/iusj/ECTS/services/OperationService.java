9d80535dca725ae52f7be673385cfcd0fdd5f115
Teguia-Abel-Pycoder <teguia.dimitri@institutsaintjean.org>
2025-02-18 10:16:25 +0100

    FEATURE: Working on the Student Operation APIs integration

--- src/main/java/iusj/ECTS/services/OperationService.java
+++ src/main/java/iusj/ECTS/services/OperationService.java
@@ -1,11 +1,14 @@
 package iusj.ECTS.services;
 
 import iusj.ECTS.models.Operation;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OperationService {
     Operation createOperation(Operation operation);
     Operation saveOperation(Operation operation);
     List<Operation> getAllOperation();
+    ResponseEntity<String> computeOperation(Long id, Map<String, String> newCourses);
 }
--- src/main/java/iusj/ECTS/services/implementation/OperationImplementation.java
+++ src/main/java/iusj/ECTS/services/implementation/OperationImplementation.java
@@ -6,10 +6,13 @@ import iusj.ECTS.services.EquivalenceService;
 import iusj.ECTS.services.ExcelHandler;
 import iusj.ECTS.services.OperationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class OperationImplementation implements OperationService {
@@ -46,4 +49,28 @@ public class OperationImplementation implements OperationService {
 
         return operationRepository.findAll();
     }
+
+    @Override
+    public ResponseEntity<String> computeOperation(Long id, Map<String, String> newCourses) {
+        Optional<Operation> optionalOperation = operationRepository.findById(id);
+        if (optionalOperation.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Operation not found");
+        }
+
+        Operation operation = optionalOperation.get();
+        
+        String classType = "";
+
+        if (operation.getClassLevel().toString().startsWith("SRT")) {
+            classType = "srt";
+        } else if (operation.getClassLevel().toString().startsWith("ISI")) {
+            classType = "isi";
+        }
+        Map<String, String> translatedCourses = equivalenceService.convertEquivalences(operation.getGrades(), operation.getSchoolName(), operation.getClassLevel() , classType);
+        Map<String, Double> result = excelHandler.mainFunction(operation.getClassLevel(), operation.getSemester(), true, translatedCourses, operation.getStudentMgp());
+        operation.setResult(result);
+        return null;
+    }
+
+
 }
